18a20d4022bd04dca67a7feeef2aa3ed87ed470f
fbyesicantrs@gmail.com <fbyesicantrs@gmail.com>
2023-08-24 02:01:11 +0300

    bot oluşturuldu

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
-from colorama import Fore, Back, Style
+import telebot
 
-print(Fore.CYAN + Back.MAGENTA + "Merhaba Ben Emre Tarafından Kodlandım" + Fore.RESET)
+bot = telebot.TeleBot("5928422327:AAGSlHRWHSf3kr4j4oTWuIJG_AN8reOegbo")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Merhaba!")
+
+bot.polling()
