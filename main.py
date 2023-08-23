81b9b5477a50ae7a59d979e77f5b6173357854f1
fbyesicantrs@gmail.com <fbyesicantrs@gmail.com>
2023-08-23 20:13:48 +0300

    kod yazildi

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+pasword = "1234"
+username = "emre"
 
+user = input("kullanıcı:")
+passd = input("parola:")
+
+if username and pasword == user and passd:
+  print("giriş başarılı ")
+  
+else:
+  print("hatalı giriş")
